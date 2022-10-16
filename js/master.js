9b3988497017b1e14560ebd5dad1d476deae361c
saeed-m-elsayed <isaeedeagle@gmail.com>
2022-10-16 10:27:14 +0200

    new edits

--- js/master.js
+++ js/master.js
@@ -27,3 +27,75 @@ form.addEventListener("submit", e => {
 });
 
 // end sheetdb api form
+
+// start sticky
+let sticky = document.getElementById("sticky-footer")
+
+window.onscroll = function(){
+    if(scrollY >= 400){
+        sticky.style.opacity = '1'
+        sticky.style.bottom = '15px'
+    }
+    else{
+            sticky.style.opacity = '0'
+            sticky.style.bottom = '-50px'
+    }
+}
+// end sticky
+
+
+// start count down
+    
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
+h.value = 1
+m.value = 59
+s.value = 59
+//store a reference to the startTimer variable
+var startTimer = null;
+
+window.onload = function timeReady(){
+    //initialize the variable
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+}
+
+// reset.addEventListener('click', function(){
+//     h.value = 0;
+//     m.value = 0;
+//     s.value = 0;
+//     //stop the timer after pressing "reset"
+//     stopInterval()
+// })
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+    return;
+}
+
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+    clearInterval(startTimer);
+}
+// end count down
